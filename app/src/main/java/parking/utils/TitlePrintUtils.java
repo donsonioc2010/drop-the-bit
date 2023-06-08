252b2c54ef6d0a67569ca38e135268277b017f4d
donsonioc2010 <whddnjs822@gmail.com>
2023-06-08 14:33:42 +0900

    feat : Print 항목 추가

--- app/src/main/java/parking/utils/TitlePrintUtils.java
+++ app/src/main/java/parking/utils/TitlePrintUtils.java
@@ -1,6 +1,21 @@
 package parking.utils;
 
 public class TitlePrintUtils {
+	public static void printMenu() {
+		processStartTitle();
+		System.out.println("0. 메뉴 출력");
+		System.out.println("1. 입차량 추가");
+		System.out.println("2. 입차 차량 단건 출력");
+		System.out.println("3. 입차 차량 전체 출력");
+		System.out.println("4. 입차 차량 정보 수정");
+		System.out.println("5. 입차 차량 정보 삭제");
+		System.out.println("6. 프로세스 종료");
+	}
+
+	public static void printSelectMenu() {
+		System.out.println("없는 메뉴입니다.");
+	}
+
 	public static void processStartTitle() {
 		System.out.println("차량 목록 관리 시스템");
 		System.out.println("--------------------------------------------------------------------");
