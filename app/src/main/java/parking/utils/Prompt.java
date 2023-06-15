74a49f533eb7c5094e3c18a176d92f5acca7c5db
donsonioc2010 <whddnjs822@gmail.com>
2023-06-15 17:27:33 +0900

    feat : Prompt Exception추가 feat : Log 색상 Static추가 및 Error기능 추가 chore : checkstyle Rule 수정 feat : MainProcess 삭제로인한 App변경

--- app/src/main/java/parking/utils/Prompt.java
+++ app/src/main/java/parking/utils/Prompt.java
@@ -3,25 +3,30 @@ package parking.utils;
 import java.util.Scanner;
 
 public class Prompt {
-	private static Scanner sc = null;
+    private static Scanner sc = null;
 
-	public static String getInputString(String title) {
-		System.out.print(title);
-		return sc.nextLine();
-	}
+    public static String getInputString(String title) {
+        System.out.print(title);
+        return sc.nextLine();
+    }
 
-	public static int getInputInt(String title) {
-		return Integer.parseInt(getInputString(title));
-	}
+    public static int getInputInt(String title) {
+        try {
+            return Integer.parseInt(getInputString(title));
+        } catch (NumberFormatException e) {
+            System.out.println(Log.getError(Log.getConsoleRedColorToKeyword("입력") + "를 " + Log.getConsoleBlueColorToKeyword("숫자만 ") + "입력해 주시기 바랍니다."));
+            return -1;
+        }
+    }
 
-	public static void scClose() {
-		sc.close();
-	}
+    public static void scClose() {
+        sc.close();
+    }
 
-	public static void scOpen() {
-		if (sc == null) {
-			sc = new Scanner(System.in);
-		}
-	}
+    public static void scOpen() {
+        if (sc == null) {
+            sc = new Scanner(System.in);
+        }
+    }
 
 }
