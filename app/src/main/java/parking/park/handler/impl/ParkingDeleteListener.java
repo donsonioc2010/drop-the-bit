e4c1c522610203a8bb9c7ad582d7bf3774394aa4
donsonioc2010 <whddnjs822@gmail.com>
2023-07-03 09:46:06 +0900

    refactor : message Log수정

--- app/src/main/java/parking/park/handler/impl/ParkingDeleteListener.java
+++ app/src/main/java/parking/park/handler/impl/ParkingDeleteListener.java
@@ -7,6 +7,7 @@ import parking.park.handler.AbstractParkingListener;
 import parking.park.utils.ParkingConstants;
 import parking.session.LoginSession;
 import parking.utils.common.console.BreadcrumbPrompt;
+import parking.utils.log.Log;
 
 public class ParkingDeleteListener extends AbstractParkingListener {
 	public ParkingDeleteListener(List<ParkingCar> list) {
@@ -25,6 +26,6 @@ public class ParkingDeleteListener extends AbstractParkingListener {
 			parkingInfoList.remove(removeCar);
 		}
 
-		System.out.println("[INFO] 직원또는 관리자만 데이터의 삭제가 가능합니다. 로그인 해주시기 바랍니다");
+		System.out.println(Log.error("직원또는 관리자만 데이터의 삭제가 가능합니다. 로그인 해주시기 바랍니다"));
 	}
 }
